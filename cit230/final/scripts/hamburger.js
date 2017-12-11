2bb5311835516e50139d1c49496b59421db7e9d6
hunteremmal <31933956+hunteremmal@users.noreply.github.com>
2017-12-11 16:07:09 -0700

    Hamburger Working
    12/11

--- cit230/final/scripts/hamburger.js
+++ cit230/final/scripts/hamburger.js
@@ -1,3 +1,3 @@
 function toggleHam() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
